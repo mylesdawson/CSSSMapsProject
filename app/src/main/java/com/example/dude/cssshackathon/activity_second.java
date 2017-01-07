7493a0c33da89f291bf4380bbcdafe38c0b9c254
mylesdawson <mylesdawson@gmail.com>
2017-01-07 15:04:59 -0800

    working buttons

--- app/src/main/java/com/example/dude/cssshackathon/activity_second.java
+++ app/src/main/java/com/example/dude/cssshackathon/activity_second.java
@@ -1,8 +1,28 @@
 package com.example.dude.cssshackathon;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class activity_second extends AppCompatActivity {
+
+    public Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btn = (Button) findViewById(R.id.newGameButton);
+
+    }
+
+    public void startNewGame(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
 }
