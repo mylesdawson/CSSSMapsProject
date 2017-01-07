d587b9e63c78a92a563217ea37e65dc98b764ec2
MikeCaxi <pancax@hotmail.com>
2017-01-07 13:33:11 -0800

    added character class

--- app/src/main/java/Character.java
+++ app/src/main/java/Character.java
@@ -3,4 +3,32 @@
  */
 
 public class Character {
+    private int hp;
+    private int ap;
+    private int xp;
+    private int lvl;
+    private int xpToNext;
+    private String[] ability = new String[3];
+    private int hpPot;
+    private int apPot;
+    private int dart;
+    private int gold;
+
+    public Character(){
+        hp = 100;
+        ap = 100;
+        xp = 0;
+        lvl = 0;
+        xpToNext = 100;
+        ability[0] = "\0";
+        ability[1] = "\0";
+        ability[2] = "\0";
+        hpPot = 5;
+        apPot = 5;
+        dart = 10;
+        gold = 0;
+    }
+
+    pu
+
 }
