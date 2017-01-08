7036432f450774df631a62cec4ed2bc8d0e56ee4
MikeCaxi <pancax@hotmail.com>
2017-01-08 03:43:47 -0800

    super simple combat

--- app/src/main/java/com/example/dude/cssshackathon/EncounterActivity.java
+++ app/src/main/java/com/example/dude/cssshackathon/EncounterActivity.java
@@ -13,238 +13,44 @@ import java.util.Random;
 
 public class EncounterActivity extends AppCompatActivity {
     //public static Game game = new Game("Hello.");
-    public String latestAction;
-    String mobName;
-    int[] mob;
+    public static int playerHP = 100;
+    public static int playerLVL = 1;
+    //public static int playerGold = 0;
+    int action = -1;
     Button attack;
-    Button abilities;
-    Button items;
     Button run;
-    ImageView mobImg;
-    ImageView playerImg;
-    TextView mobInfo;
-    TextView playerInfo;
-    int att;
-    int hp = 100;
-    public HashMap<String, int[]> mobMap = new HashMap<String, int[]>();
-    //ame game;
+    HashMap<String, int[]> mobs = new HashMap();
+    HashMap<Integer, String> numMobs = new HashMap();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encounter);
-     //   game.encounter();
         attack = (Button)findViewById(R.id.attack);
-    //    abilities = (Button)findViewById(R.id.abilities);
-    //    items = (Button)findViewById(R.id.shopB);
-    //    run = (Button)findViewById(R.id.run);
-        mobImg = (ImageView)findViewById(R.id.mobImg);
-        playerImg = (ImageView)findViewById(R.id.playerImg);
-        mobInfo = (TextView)findViewById(R.id.mobInfo);
-        playerInfo = (TextView)findViewById(R.id.playerInfo);
-        //game = activity_second.game;
-
-        int[][] arr = new int[5][7];
-        arr[0][0] = 100;
-        arr[0][1] = 5;
-        arr[0][2] = 25;
-        arr[0][3] = 10;
-        arr[0][4] = 2;
-        arr[0][5] = 2;
-        arr[0][6] = 2;
-        arr[1][0] = 100;
-        arr[1][1] = 5;
-        arr[1][2] = 25;
-        arr[1][3] = 10;
-        arr[1][4] = 2;
-        arr[1][5] = 2;
-        arr[1][6] = 2;
-        arr[2][0] = 100;
-        arr[2][1] = 5;
-        arr[2][2] = 25;
-        arr[2][3] = 10;
-        arr[2][4] = 2;
-        arr[2][5] = 2;
-        arr[2][6] = 2;
-        arr[2][0] = 100;
-        arr[2][1] = 5;
-        arr[2][2] = 25;
-        arr[2][3] = 10;
-        arr[2][4] = 2;
-        arr[2][5] = 2;
-        arr[2][6] = 2;
-        arr[3][0] = 100;
-        arr[3][1] = 5;
-        arr[3][2] = 25;
-        arr[3][3] = 10;
-        arr[3][4] = 2;
-        arr[3][5] = 2;
-        arr[3][6] = 2;
-        arr[4][0] = 100;
-        arr[4][1] = 5;
-        arr[4][2] = 25;
-        arr[4][3] = 10;
-        arr[4][4] = 2;
-        arr[4][5] = 2;
-        arr[4][6] = 2;
-        mobMap.put("wraith", arr[0]);
-        mobMap.put("wraithhot", arr[1]);
-        mobMap.put("wraithcold", arr[2]);
-        mobMap.put("wraithbright", arr[3]);
-        mobMap.put("wraithdark", arr[4]);
-
-        Random generator = new Random();
-        Object[] getmobs = mobMap.values().toArray();
-//       Toast.makeText(this, getmobs.length, Toast.LENGTH_SHORT).show();
-        int ran = generator.nextInt(getmobs.length);
-        int[] randomMob = (int[]) getmobs[ran];
-        mobName = "wraith";
-        mob = randomMob;
-
-       int mobBaseHP = mob[0];
-        mobInfo.setText( mobName+ "\nhp: " + mob[0] +"|" + mobBaseHP + "\nattack: " + mob[1]);
-        playerInfo.setText("you" + "\nhp: " + hp);// + "|" + hpMax + "\nap: " + ap + "|" + apMax);
-        int id = getResources().getIdentifier("@mipmap-hdpi/" + mobName, null, null);
-        mobImg.setImageResource(id);
-        encounter();
+        run = (Button)findViewById(R.id.run);
+        battle();
     }
 
-    public void attackSetText(View v){
-        latestAction = attack.getText().toString();
-
-    }
-  /*  public void abilitiesSetText(){
-        if(abilities.getText().toString().equals("abilities")){
-            attack.setText(game.ability[0]);
-            abilities.setText(game.ability[1]);
-            items.setText(game.ability[2]);
-            run.setText("back");
-            return;
-        }
-        latestAction = abilities.getText().toString();
-        attack.setText("attack");
-        abilities.setText("abilities");
-        items.setText("items");
-        run.setText("run");
-    }
-    public void itemsSetText(){
-        if(items.getText().toString().equals("items")){
-            attack.setText("hp potion: " + game.hpPot);
-            abilities.setText("dart: " + game.dart);
-            items.setText("ap potion" + game.apPot);
-            run.setText("back");
-            return;
-        }
-        latestAction = items.getText().toString();
-        attack.setText("attack");
-        abilities.setText("abilities");
-        items.setText("items");
-        run.setText("run");
-    }
-    */
-    public void runSetText(View v){
-        /*if(items.getText().toString().equals("back")){
-            latestAction = "back";
-            attack.setText("attack");
-            abilities.setText("abilities");
-            items.setText("items");
-            run.setText("run");
-            return;
-        }*/
-        finish();
-    }
-
-    public void encounter() {
-        //Random generator = new Random();
-        //Object[] mobs = mobMap.values().toArray();
-        //String randomMob = (String) mobs[generator.nextInt(mobs.length)];
-        //int[]  mob = mobMap.get(randomMob);
-        //EncounterActivity.setInfo(randomMob, mob);
-        while(mob[0] > 0 && hp > 0) {
-            update();
-            combatAction(mob);
-            update();
-            if(mob[0] > 0) hp -= mob[1];
-            update();
-        }
-       /* if(game.hp > 0){
-            game.xp += game.mob[2];
-            game.gold += game.mob[3];
-            game.hpPot += game.mob[4];
-            game.apPot += game.mob[5];
-            game.dart += game.mob[6];
-            Toast.makeText(this, "You rekt that " + game.mobName + "!", Toast.LENGTH_SHORT).show();
-            game.lvlUp();
-            game.save();
-            finish();
-        }*/
-    }
-    private void combatAction(int[] mob){
-        boolean resolved = false;
-        latestAction = "";
+    private void battle(){
         Random ran = new Random();
-        while(!resolved) {
-            if (latestAction.equals("attack")) {
-                int dmg = ran.nextInt();
-                mob[0] -= dmg;
-                Toast.makeText(this, "You Did " + dmg + "damage!", Toast.LENGTH_SHORT).show();
-                resolved = true;
-                continue;
-            }
-        /*    if (latestAction.equals(game.ability[0])){
-                skill(0, mob);
-                resolved = true;
-                continue;
-            }
-            if (latestAction.equals(game.ability[1])){
-                skill(1, mob);
-                resolved = true;
-                continue;
-            }
-            if (latestAction.equals(game.ability[2])){
-                skill(2, mob);
-                resolved = true;
-                continue;
-            }
-            if(latestAction.equals("hp potion") && game.hpPot > 0){
-                game.hp += game.hpMax;
-                game.hpPot--;
-            }
-            if(latestAction.equals("ap potion") && game.apPot > 0){
-                game.ap += game.apMax;
-                game.apPot--;
-            }
-            if(latestAction.equals("dart") && game.dart > 0){
-                mob[0] -= 5;
-                game.dart--;
-            }
-            //if(action.equals("back")) Page3.Back(); */
+        String randomMob = numMobs.get(ran.nextInt(numMobs.size()));
+        int[] mobStat = mobs.get(randomMob);
+        while(mobStat[0] > 0 && playerHP > 0) {
+            while (action == -1);
+            if (action == 1) mobStat[0] -= ran.nextInt(mobStat[0]) + playerLVL;
+            if (action == 4) finish();
+            playerHP -= mobStat[1];
+            action = -1;
         }
+        if(playerHP > 0){
+            playerLVL++;
+        }
+        else Toast.makeText(this, "You Died", Toast.LENGTH_SHORT).show();
     }
 
-   /* private void skill(int num, int[] mob){
-        String use = game.ability[num];
-        if(use.equals("strong attack: 10") && game.ap >= 10){
-            mob[0] -= 15;
-            game.ap -= 10;
-            return;
-        }
-        if(use.equals("life steal: 20") && game.ap >= 20){
-            mob[0] -= 5;
-            game.hp+= 25;
-            game.ap -= 20;
-            return;
-        }
-        if(use.equals("double strike: 50") && game.ap >= 50){
-            game.ap -= 50;
-            combatAction(mob);
-            mob[0] -= 10;
-            return;
-        }
-        Toast.makeText(this, "Not enough AP, you have wasted a turn.", Toast.LENGTH_SHORT).show();
-    }*/
-
-    private void update(){
-        mobInfo.setText(mobName + "\nhp: " + mob[0] + "\nattack: " + mob[1]);
-        playerInfo.setText("you" + "\nhp: " + hp);// + "|" + game.hpMax + "\nap: " + game.ap + "|" + game.apMax);
+    public void attLis(){
+        action = 1;
+    }
+    public void runLis(){
+        action = 0;
     }
 }
