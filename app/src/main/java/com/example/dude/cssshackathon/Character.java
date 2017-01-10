6e37014a227b8864989406615a3a65e4f28a3bf5
MikeCaxi <pancax@hotmail.com>
2017-01-09 16:03:21 -0800

    cleaned up scrapped code

---
+++ app/src/main/java/com/example/dude/cssshackathon/Character.java
@@ -0,0 +1,27 @@
+package com.example.dude.cssshackathon;
+
+/**
+ * Created by panca on 2017-01-09.
+ */
+
+public class Character {
+    public static String name;
+    public static long hp;
+    public static long hpMax;
+    public static long ap;
+    public static long apMax;
+    public static long lvl;
+    public static long ammo;
+    public static long hpPot;
+    public static long apPot;
+    public static String[] abilities = new String[3];
+
+    /*
+    public static int str;
+    public static int dex;
+    public static String[] status;
+    public static int luk;
+    public static int end;
+    public static Character[]
+     */
+}
---
+++ app/src/main/java/com/example/dude/cssshackathon/PartyMember.java
@@ -0,0 +1,28 @@
+package com.example.dude.cssshackathon;
+
+/**
+ * Created by panca on 2017-01-09.
+ */
+
+public class PartyMember {
+    public long hp;
+    public long hpMax;
+    public int hpPot;
+    public String[] abilities = new String[4];
+}
+
+class assault extends PartyMember{
+
+}
+
+class tank extends PartyMember{
+
+}
+
+class medic extends PartyMember{
+
+}
+
+class support extends PartyMember{
+
+}
